e9c9fd1c8cce2e459bac4d3f853268b76145d8d2
SarahOnoja <linksarahonoja@gmail.com>
2024-04-03 14:18:05 +0100

    first

---
+++ src/Flexbox.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Flexbox.css"
+
+function Flexbox(){
+    return(
+        
+        <div class = "gridContainer">
+            <h1 class = "grid1">python</h1>
+            <h1 class = "grid2">C</h1>
+            <h1 class = "grid3">Java</h1>
+            <h1 class = "grid4">Perl</h1>
+            <h1 class = "grid5">Ruby</h1>
+            <h1 class = "grid6">pascal</h1>
+            <h1 class = "grid7">C++</h1>
+            <h1 class = "grid8">Javascript</h1>
+            <h1 class = "grid9">GO</h1>
+            <h1 class = "grid10">Kotlin</h1>
+            <h1 class = "grid11">PHP</h1>
+            <h1 class = "grid12">Swift</h1>
+            <h1 class = "grid13">C#</h1>
+            <h1 class = "grid14">Typescript</h1>
+            <h1 class = "grid15">Matlab</h1>
+            <h1 class = "grid16">Scala</h1>
+        </div>
+    );
+}
+export default Flexbox;
